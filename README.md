aa8dcbfe9586a01ced0f464c7ae6a3dfdf0e7a18
Nitin <nitinpillai30@yahoo.com>
2021-08-20 20:57:40 +0700

    Adding authentication
    Using JWT & cookie session to setup a test demo for authentication

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Ticketing manager
 
 ## Work in progress!
+
+This Ticketing project is built around the implementation of Microservices.
+
+1. Docker
+2. Kubernetes
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express'
 import { body, validationResult } from 'express-validator'
+import jwt from 'jsonwebtoken'
 import { User } from '../models/user'
 import { RequestValidationError } from '../errors/request-validation-error'
 import { BadRequestError } from '../errors/BadRequestError'
@@ -31,6 +32,23 @@ router.post(
 
     const user = User.build({ email, password })
     await user.save()
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+      },
+      'shouldBeInEnvVariableTestOnly'
+    )
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt,
+      isNew: true,
+      isChanged: false,
+      isPopulated: false,
+    }
 
     res.status(201).send(user)
   }
