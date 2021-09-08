55195f3befae631e5c4f379cf48ee1f150c107d7
Nitin Pillai <39956893+Nitin3021@users.noreply.github.com>
2021-09-08 18:45:47 +0700

    Adding payments service to the project
    Payments service will take responsibility of keeping track and
    processing payments on the orders placed. Setting up payment module in
    this commit.

---
+++ payments/src/test/setup.ts
@@ -0,0 +1,53 @@
+import { MongoMemoryServer } from 'mongodb-memory-server'
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+
+declare global {
+  var signin: () => string[]
+}
+
+jest.mock('../nats-wrapper')
+
+let mongo: any
+beforeAll(async () => {
+  process.env.JWT_KEY = 'testingpurposeonly!'
+
+  mongo = await MongoMemoryServer.create()
+
+  await mongoose.connect(mongo.getUri(), {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+})
+
+beforeEach(async () => {
+  jest.clearAllMocks()
+  const collections = await mongoose.connection.db.collections()
+
+  for (let collection of collections) {
+    await collection.deleteMany({})
+  }
+})
+
+afterAll(async () => {
+  await mongo.stop()
+  await mongoose.connection.close()
+})
+
+global.signin = () => {
+  // Build a JWT payload. {id, email}
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: 'test@testing.com'
+  }
+
+  // Creating JWT.
+  const token = jwt.sign(payload, process.env.JWT_KEY!)
+  const session = { jwt: token }
+  const sessionJSON = JSON.stringify(session)
+  const base64 = Buffer.from(sessionJSON).toString('base64')
+
+  // return a string thats the cookie with the encoded data.
+  return [`express:sess=${base64}`]
+
+}
