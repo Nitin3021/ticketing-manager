e9b994189b2b4302a2164843d5f24f1cdd3b3001
Nitin Pillai <39956893+Nitin3021@users.noreply.github.com>
2021-09-10 08:55:07 +0700

    Changing client to incorporate new Services
    Integrating Tickets service to the client handler. Creating new route
    /tickets/new.js which will display form and integrate it with Tickets
    service.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,25 +1,30 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
-import buildClient from "../api/buildClient";
-import Header from "../components/Header";
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/buildClient'
+import Header from '../components/Header'
 
 const AppComponent = ({ Component, pageProps, currentUser }) => {
   return (
     <div>
       <Header currentUser={currentUser} />
-      <Component {...pageProps} />
+      <div className='container'>
+        <Component currentUser={currentUser} {...pageProps} />
+      </div>
     </div>
-  );
-};
+  )
+}
 
 AppComponent.getInitialProps = async (appContext) => {
   const client = buildClient(appContext.ctx)
   const { data } = await client.get('/api/users/currentuser')
 
   let pageProps = {}
-
   if (appContext.Component.getInitialProps) {
-    pageProps = await appContext.Component.getInitialProps(appContext.ctx)
+    pageProps = await appContext.Component.getInitialProps(
+      appContext.ctx,
+      client,
+      data.currentUser
+    )
   }
 
   return {
@@ -28,4 +33,4 @@ AppComponent.getInitialProps = async (appContext) => {
   }
 }
 
-export default AppComponent;
+export default AppComponent
