2fc1ef0ccbc6d957a36aa8f4d9300f66ae6c5130
Nitin Pillai <39956893+Nitin3021@users.noreply.github.com>
2021-09-11 09:03:04 +0700

    All services have been implemented in Client
    Changes have been made to the client so that it can handle all defined
    routes from tickets, orders and payments. Stripe payment successfully
    takes place after an order has been placed while redirecting the user to
    appropriate page.

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,33 +1,35 @@
-import React from "react";
-import Link from "next/link";
+import React from 'react'
+import Link from 'next/link'
 
 const Header = ({ currentUser }) => {
   const links = [
-    !currentUser && { label: "Sign up", href: "/auth/signup" },
-    !currentUser && { label: "Sign in", href: "/auth/signin" },
-    currentUser && { label: "Sign out", href: "/auth/signout" },
+    !currentUser && { label: 'Sign up', href: '/auth/signup' },
+    !currentUser && { label: 'Sign in', href: '/auth/signin' },
+    currentUser && { label: 'Sell Tickets', href: '/tickets/new' },
+    currentUser && { label: 'My Orders', href: '/orders' },
+    currentUser && { label: 'Sign out', href: '/auth/signout' }
   ]
     .filter((linkConfig) => linkConfig)
     .map(({ label, href }) => {
       return (
-        <li key={href} className="nav-item">
+        <li key={href} className='nav-item'>
           <Link href={href}>
-            <a className="nav-link">{label}</a>
+            <a className='nav-link'>{label}</a>
           </Link>
         </li>
-      );
-    });
+      )
+    })
 
   return (
-    <nav className="navbar navbar-light bg-light">
-      <Link href="/">
-        <a className="navbar-brand">GitTix</a>
+    <nav className='navbar navbar-light bg-light'>
+      <Link href='/'>
+        <a className='navbar-brand'>GitTix</a>
       </Link>
-      <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">{links}</ul>
+      <div className='d-flex justify-content-end'>
+        <ul className='nav d-flex align-items-center'>{links}</ul>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,7 +1,18 @@
 import { useEffect, useState } from 'react'
+import StripeCheckout from 'react-stripe-checkout'
+import Router from 'next/router'
+import useRequest from '../../hooks/useRequest'
 
-const OrderShow = ({ order }) => {
+const OrderShow = ({ order, currentUser }) => {
   const [timeLeft, setTimeLeft] = useState(0)
+  const { doRequest, errors } = useRequest({
+    url: '/api/payments',
+    method: 'post',
+    body: {
+      orderId: order.id
+    },
+    onSuccess: (payment) => Router.push('/orders')
+  })
 
   useEffect(() => {
     const findTimeLeft = () => {
@@ -21,7 +32,18 @@ const OrderShow = ({ order }) => {
     return <div>Order Expired</div>
   }
 
-  return <div>Time left to pay: {timeLeft} seconds</div>
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={({ id }) => doRequest({ token: id })}
+        stripeKey={process.env.REACT_APP_STRIPE_PUB_KEY}
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+      {errors}
+    </div>
+  )
 }
 
 OrderShow.getInitialProps = async (context, client) => {
